f9750aed3d80e93bd5dfec3de38ca84f257eadd5
farhah din <farhahdin97@hotmail.com>
2022-03-17 11:04:53 +0000

    declaring

---
+++ index.js
@@ -0,0 +1,7 @@
+const cTable = require ('console.table');
+const inquirer = require ('inquirer');
+const mysql = require ('mysql');
+const commaNumber = require('comma-number');
+const Department = require(__dirname + '/classes/Department.js');
+const Role = require(__dirname + '/classes/Role.js');
+const Employee = require(__dirname + '/classes/Employee.js');
