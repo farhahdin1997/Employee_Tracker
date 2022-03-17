5f6a6d4dd4f1dbd1095d95289fab62a4937dab07
farhah din <farhahdin97@hotmail.com>
2022-03-17 12:11:25 +0000

    /*Entering this data into each table under each atribute*/

---
+++ db/seed.sql
@@ -0,0 +1,28 @@
+USE company_db;
+/*Entering this data into each table under each atribute*/
+INSERT INTO departments(name)
+VALUES('Sales'),
+('Engineering'),
+('Finance')
+('Legal');
+
+INSERT INTTO roles (title, salary,department_id)
+VALUES ('Sales Leaad', 20000, 1),
+('Salesperson' , 10000, 1),
+('Lead Engineer', 35000, 2),
+('Software Engineer', 25000, 2),
+('Account Manager', 30000,3),
+('Accountant', 25000,),
+('Lawyer', 40000 ,4);
+
+
+INSERT INTO employees(first_name, last_name, role_id, manager_id)
+VALUES('Karl', 'Mullins', 1,  NULL),
+('Aisha', 'Din', 2 ,1),
+('Bily', 'Bhambi',2,1),
+('Himaas', 'Alyy', 3, NULL),
+('Sadaf', 'Din', 4, 3),
+('Isma', 'Hazel', 4,3),
+('Ella', 'Willson', 5 ,NULL),
+('Daniele', 'Spark', 6, 5),
+('Kimmy', 'Schmitt',7, NULL);
